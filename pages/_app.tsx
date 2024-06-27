b380947fca7e4eadd57bc574e72cf0da7dccd71a
Aknur Kassym <kassym.aknur@gmail.com>
2024-06-27 17:31:28 +0400

    feat: unable light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "nextra-theme-blog/style.css";
 import type { AppProps } from "next/app";
 import { GeistSans } from 'geist/font/sans';
+import { ThemeProvider } from "next-theme";
 import "../styles/main.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={GeistSans.className}>
-      <Component {...pageProps} />
-    </main>
+    <ThemeProvider darkTheme="dark" defaultTheme="dark" setAttribute={false}>
+      <main className={GeistSans.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
   );
 }
