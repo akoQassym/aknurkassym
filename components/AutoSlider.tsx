1b2d2df3831cde527da5e6c177f5646275ae65fb
Aknur Kassym <kassym.aknur@gmail.com>
2024-07-26 14:31:50 +0500

    feat: add a slider component

---
+++ components/AutoSlider.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface AutoSliderProps {
+  slidesToShow: number;
+  autoplay: boolean;
+  children: ReactNode;
+}
+
+const AutoSlider: React.FC<AutoSliderProps> = ({ slidesToShow = 1, autoplay = false, children }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    autoplay: autoplay,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+
+  return (
+    <div className="slider-container auto-slider">
+      <Slider {...settings}>
+        {children}
+      </Slider>
+    </div>
+  );
+};
+
+export default AutoSlider;
